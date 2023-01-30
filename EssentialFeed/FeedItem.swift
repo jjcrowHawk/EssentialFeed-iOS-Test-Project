9faf552881512647ed5f08dcf74b33bb165345fa
jjcrow <hawkcrowxx@gmail.com>
2023-01-30 10:18:16 -0500

    Create Feed Item

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Macbook Pro on 30/1/23.
+//
+
+import Foundation
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
