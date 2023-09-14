91c0c47a8ec380d7934f5a82c2204b2e47bfb706
jjcrow <hawkcrowxx@gmail.com>
2023-09-14 13:24:23 -0500

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Macbook Pro on 14/9/23.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    
+    
+}
