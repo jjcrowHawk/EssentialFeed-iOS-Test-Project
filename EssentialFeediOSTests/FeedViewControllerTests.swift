89a41933bbcde038939e447ccc4d760baa17e38b
jjcrow <hawkcrowxx@gmail.com>
2023-09-17 00:48:12 -0500

    Retry failed image load on retry action

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -186,6 +186,29 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual (view?.isShowingRetryAction, true, "Expected retry action once image loading completes with invalid image data")
     }
     
+    func test_feedImageViewRetryButton_retriesImageLoad() {
+        let image0 = makeImage(url: URL(string: "http//url-0.com")!)
+        let image1 = makeImage(url: URL(string: "http//url-1.com")!)
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [image0, image1])
+        
+        let view0 = sut.simulateFeedImageViewVisible(at: 0)
+        let view1 = sut.simulateFeedImageViewVisible(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url], "Expected two image URL request for the two visible views")
+        
+        loader.completeImageLoadingWithError(at: 0)
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url], "Expected only two image URL requests before retry action")
+        
+        view0?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url, image0.url], "Expected third imageURL request after view retry action")
+        
+        view1?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url, image0.url, image1.url], "Expected fourth imageURL request after second view retry action")
+    }
+    
 
     // MARK: - HELPERS
     
@@ -348,6 +371,10 @@ private extension FeedImageCell {
     var renderedImage: Data? {
         return feedImageView.image?.pngData()
     }
+    
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
 }
 
 private extension UIRefreshControl {
@@ -357,6 +384,16 @@ private extension UIRefreshControl {
                 (target as NSObject).perform(Selector($0))
             }
         })
+    }
+}
+
+private extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach({ selector in
+                (target as NSObject).perform(Selector(selector))
+            })
+        }
     }
 }
 
