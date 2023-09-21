fc39c7b658792f44e3ba318cf2ef5baaf235dab4
jjcrow <hawkcrowxx@gmail.com>
2023-09-21 13:21:39 -0500

    Dispatch background feed completion to main thread before updating the UI

--- EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -263,6 +263,20 @@ final class FeedUIIntegrationTests: XCTestCase {
         XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
     }
     
+    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        let exp = expectation(description: "Wasit for background queue")
+        DispatchQueue.global(qos: .background).async {
+            loader.completeFeedLoading(at: 0)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        
+    }
 
     // MARK: - HELPERS
     
