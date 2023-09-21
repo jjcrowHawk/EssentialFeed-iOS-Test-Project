6db033ef01ad01136daa5c13d5cd9c9b86a9e6a6
jjcrow <hawkcrowxx@gmail.com>
2023-09-21 14:30:19 -0500

    `FeedViewController` does not render errorView on load

--- EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -293,6 +293,14 @@ final class FeedUIIntegrationTests: XCTestCase {
         
         wait(for: [exp], timeout: 5.0)
     }
+    
+    func test_errorView_doesNotRenderErrorOnLoad() {
+        let (sut, _) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.errorMessage, nil)
+    }
 
     // MARK: - HELPERS
     
