771673910848b646742b8b22a32fc9282468a7b3
jjcrow <hawkcrowxx@gmail.com>
2023-09-22 11:35:12 -0500

    Replace `FeedPresenter` from iOS module with `FeedPresenter` from shared module

--- EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
+++ EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -1,14 +1,14 @@
 //
 //  FeedErrorViewModel.swift
-//  EssentialFeediOS
+//  EssentialFeed
 //
 //  Created by Macbook Pro on 22/9/23.
 //
 
 import Foundation
 
-struct FeedErrorViewModel {
-    let message: String?
+public struct FeedErrorViewModel {
+    public let message: String?
     
     static var noError: FeedErrorViewModel {
         return FeedErrorViewModel(message: nil)
