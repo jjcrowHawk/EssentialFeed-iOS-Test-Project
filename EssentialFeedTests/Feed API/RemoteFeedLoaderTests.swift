9a010e9a2595fc48538e8276cfad8a1a0a637e80
jjcrow <hawkcrowxx@gmail.com>
2023-09-22 23:14:33 -0500

    Adding implementation and tests for Remote Feed Image Loader for cross platform module

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -22,7 +22,7 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         
         sut.load{_ in }
         
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_reqeustDataFromURLTwice() {
@@ -155,29 +155,5 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         action()
         
         wait(for: [exp], timeout: 1.0)
-    }
-    
-    private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
-        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        
-        var requestedURLs: [URL] {
-            return messages.map{ $0.url }
-        }
-
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            messages.append((url, completion))
-            requestedURL = url
-        }
-        
-        func complete(with error: Error, at index: Int = 0){
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0){
-            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            
-            messages[index].completion(.success((data, response)))
-        }
     }
 }
